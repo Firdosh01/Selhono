53c3c54bc6d08ce599c984fc4fa96c80267f8d4d
Firdosh01 <firdosh7392@gmail.com>
2023-12-28 07:09:41 -0500

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      DMSerif: ["DM Serif Display"],
+      Jost: ["Jost"],
+    },
+    colors: {
+      black: "#292F36",
+      richBlack: "#4D5053",
+      white: "#FFF",
+      burlywood: {
+        50: "#CDA274",
+        100: "#D8A876",
+        150: "#C76904",
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
