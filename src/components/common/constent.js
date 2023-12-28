3c2c028a65480e61d7c45b63c4207920f588595e
Firdosh01 <firdosh7392@gmail.com>
2023-12-28 09:00:51 -0500

    implement navigation

---
+++ src/components/common/constent.js
@@ -0,0 +1,35 @@
+
+
+export const navigation = [
+    {
+        id: 1,
+        link: "Home",
+        path: "/"
+    },
+    {
+        id: 2,
+        link: "Pages",
+        path: "/pages"
+    },
+    {
+        id: 3,
+        link: "Services",
+        path: "/services"
+    },
+    {
+        id: 4,
+        link: "Project",
+        path: "/project"
+    },
+    {
+        id: 5,
+        link: "Blog",
+        path: "/blog"
+    },
+    {
+        id: 6,
+        link: "Contact",
+        path: "/contact"
+    },
+
+]
