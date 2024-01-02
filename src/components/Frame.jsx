a1a06e035aebd630f296a755e7158ca9675cc77c
Firdosh01 <firdosh7392@gmail.com>
2024-01-02 01:43:37 -0500

    implement frame reusable components

---
+++ src/components/Frame.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Frame({ frameImg, frameTitle }) {
+  return (
+    <div>
+      <div className="relative">
+        <img
+          src={frameImg}
+          alt=""
+          className="object-cover w-full lg:h-[356px] md:h-[215px] h-[170px] select-none"
+        />
+        <div className="frame_text lg:mt-[188px] mt-[70px]">
+          <h1 className="heading lg:text-[70px] sm:text-[50px] text-[30px]  text-white drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+            {frameTitle}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
